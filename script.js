a521bccaaf74d3598f69a89af38316de3d4625b3
Gyromitte <gyromitte@gmail.com>
2021-09-27 18:36:56 -0500

    Added the Operate function, that allows to select which function you want to use

--- script.js
+++ script.js
@@ -2,14 +2,27 @@ let firstNumber = 0;
 let secondNumber = 0;
 
 firstNumber = prompt("write the first number: ");
+operator = prompt("Write the name of the operator that you want to use: ");
 secondNumber = prompt("write the second number: ");
+
+function Operate(operator, firstNumber, secondNumber){
+    switch (operator){
+        case "Add": Add(firstNumber, secondNumber);
+        break;
+        case "Substract": Substract(firstNumber, secondNumber);
+        break;
+        case "Multiply": Multiply(firstNumber, secondNumber);
+        break;
+        case "Divide": Divide(firstNumber, secondNumber);
+    }
+}
 
 function Add(firstNumber, secondNumber){
     return(firstNumber + secondNumber);
 }
 
 function Substract(firstNumber, secondNumber){
-    return (firstNumber - secondNumber);
+    return console.log(firstNumber - secondNumber);
 }
 
 function Multiply(firstNumber, secondNumber){
@@ -17,7 +30,7 @@ function Multiply(firstNumber, secondNumber){
 }
 
 function Divide(firstNumber, secondNumber){
-    return console.log(firstNumber / secondNumber);
+    return (firstNumber / secondNumber);
 }
 
-Divide(firstNumber, secondNumber);
+Operate(operator, firstNumber, secondNumber);
