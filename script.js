f58e08c4fd7300270b26bcdb9d83e21d775f78b7
Gyromitte <gyromitte@gmail.com>
2021-09-29 17:01:49 -0500

    Added functionality to the AC and numerical buttons

--- script.js
+++ script.js
@@ -5,6 +5,54 @@ let secondNumber = 0;
 operator = prompt("Write the name of the operator that you want to use: ");
 secondNumber = prompt("write the second number: ");*/
 
+//Reference to elements
+const screenDisplay = document.getElementById('screen-display');
+const AC = document.getElementById('AC');
+
+const cero = document.getElementById('last-button');
+const one = document.getElementById('one');
+const two = document.getElementById('two');
+const three = document.getElementById('three');
+const four = document.getElementById('four');
+const five = document.getElementById('five');
+const six = document.getElementById('six');
+const seven = document.getElementById('seven');
+const eight = document.getElementById('eight');
+const nine = document.getElementById('nine');
+
+//Events
+cero.addEventListener('click', e=>{
+    screenDisplay.textContent = "0";
+});
+one.addEventListener('click', e=>{
+    screenDisplay.textContent = "1";
+});
+two.addEventListener('click', e=>{
+    screenDisplay.textContent = "2";
+});
+three.addEventListener('click', e=>{
+    screenDisplay.textContent = "3";
+});
+four.addEventListener('click', e=>{
+    screenDisplay.textContent = "4";
+});
+five.addEventListener('click', e=>{
+    screenDisplay.textContent = "5";
+});
+six.addEventListener('click', e=>{
+    screenDisplay.textContent = "6";
+});
+seven.addEventListener('click', e=>{
+    screenDisplay.textContent = "7";
+});
+eight.addEventListener('click', e=>{
+    screenDisplay.textContent = "8";
+});
+nine.addEventListener('click', e=>{
+    screenDisplay.textContent = "9";
+});
+
+
 function Operate(operator, firstNumber, secondNumber){
     switch (operator){
         case "Add": Add(firstNumber, secondNumber);
@@ -33,4 +81,3 @@ function Divide(firstNumber, secondNumber){
     return (firstNumber / secondNumber);
 }
 
-Operate(operator, firstNumber, secondNumber);
