d7b9c30be136011ff405b6849edb850c5403a8f3
Gyromitte <gyromitte@gmail.com>
2021-09-29 18:13:12 -0500

    Added functionality to the symbols button

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-let firstNumber = 0;
 let secondNumber = 0;
 
 /*firstNumber = prompt("write the first number: ");
 operator = prompt("Write the name of the operator that you want to use: ");
 secondNumber = prompt("write the second number: ");*/
 
-//Reference to elements
 const screenDisplay = document.getElementById('screen-display');
+let firstNumber = screenDisplay.textContent;
 const AC = document.getElementById('AC');
+const symbols = document.getElementById('symbols');
 
 const cero = document.getElementById('last-button');
 const one = document.getElementById('one');
@@ -20,36 +20,61 @@ const seven = document.getElementById('seven');
 const eight = document.getElementById('eight');
 const nine = document.getElementById('nine');
 
+
+
 //Events
 cero.addEventListener('click', e=>{
-    screenDisplay.textContent = "0";
+    screenDisplay.textContent += "0";
+    firstNumber = screenDisplay.textContent;
 });
 one.addEventListener('click', e=>{
-    screenDisplay.textContent = "1";
+    screenDisplay.textContent += "1";
+    firstNumber = screenDisplay.textContent;
 });
 two.addEventListener('click', e=>{
-    screenDisplay.textContent = "2";
+    screenDisplay.textContent += "2";
+    firstNumber = screenDisplay.textContent;
 });
 three.addEventListener('click', e=>{
-    screenDisplay.textContent = "3";
+    screenDisplay.textContent += "3";
+    firstNumber = screenDisplay.textContent;
 });
 four.addEventListener('click', e=>{
-    screenDisplay.textContent = "4";
+    screenDisplay.textContent += "4";
+    firstNumber = screenDisplay.textContent;
 });
 five.addEventListener('click', e=>{
-    screenDisplay.textContent = "5";
+    screenDisplay.textContent += "5";
+    firstNumber = screenDisplay.textContent;
 });
 six.addEventListener('click', e=>{
-    screenDisplay.textContent = "6";
+    screenDisplay.textContent += "6";
+    firstNumber = screenDisplay.textContent;
 });
 seven.addEventListener('click', e=>{
-    screenDisplay.textContent = "7";
+    screenDisplay.textContent += "7";
+    firstNumber = screenDisplay.textContent;
 });
 eight.addEventListener('click', e=>{
-    screenDisplay.textContent = "8";
+    screenDisplay.textContent += "8";
+    firstNumber = screenDisplay.textContent;
 });
 nine.addEventListener('click', e=>{
-    screenDisplay.textContent = "9";
+    screenDisplay.textContent += "9";
+    firstNumber = screenDisplay.textContent;
+});
+
+//Operators
+AC.addEventListener('click', e=>{
+    screenDisplay.textContent = "";
+});
+
+symbols.addEventListener('click', e=>{
+    if(screenDisplay.textContent[0] == "-"){
+        screenDisplay.textContent = screenDisplay.textContent.substring(1);
+    }else{
+        screenDisplay.textContent = screenDisplay.textContent[0] = "-" + screenDisplay.textContent; 
+    }
 });
 
 
