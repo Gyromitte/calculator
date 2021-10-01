49103f02aef2cd5c260b3574024a81bf366da822
Gyromitte <gyromitte@gmail.com>
2021-09-30 19:34:29 -0500

    Added the dot button, capped the limit of numbers the user can input and finished the project

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ const equal = document.getElementById('equal');
 const multiply = document.getElementById('multiply');
 const divide = document.getElementById('divide');
 const modular = document.getElementById('modular');
+const dot = document.getElementById('dot');
 
 const cero = document.getElementById('last-button');
 const one = document.getElementById('one');
@@ -23,62 +24,92 @@ const seven = document.getElementById('seven');
 const eight = document.getElementById('eight');
 const nine = document.getElementById('nine');
 
-
-
 //Events
-cero.addEventListener('click', e=>{
-    screenDisplay.textContent += "0";
-    secondNumber = screenDisplay.textContent;
-});
-one.addEventListener('click', e=>{
-    screenDisplay.textContent += "1";
-    secondNumber = screenDisplay.textContent;
-});
-two.addEventListener('click', e=>{
-    screenDisplay.textContent += "2";
-    secondNumber = screenDisplay.textContent;
-});
-three.addEventListener('click', e=>{
-    screenDisplay.textContent += "3";
-    secondNumber = screenDisplay.textContent;
-});
-four.addEventListener('click', e=>{
-    screenDisplay.textContent += "4";
-    secondNumber = screenDisplay.textContent;
-});
-five.addEventListener('click', e=>{
-    screenDisplay.textContent += "5";
-    secondNumber = screenDisplay.textContent;
-});
-six.addEventListener('click', e=>{
-    screenDisplay.textContent += "6";
-    secondNumber = screenDisplay.textContent;
-});
-seven.addEventListener('click', e=>{
-    screenDisplay.textContent += "7";
-    secondNumber = screenDisplay.textContent;
-});
-eight.addEventListener('click', e=>{
-    screenDisplay.textContent += "8";
-    secondNumber = screenDisplay.textContent;
-});
-nine.addEventListener('click', e=>{
-    screenDisplay.textContent += "9";
-    secondNumber = screenDisplay.textContent;
-});
+    cero.addEventListener('click', e=>{
+        if(screenDisplay.textContent.length < 12){
+            screenDisplay.textContent += "0";
+        }
+        secondNumber = screenDisplay.textContent;
+    });
+    one.addEventListener('click', e=>{
+        if(screenDisplay.textContent.length < 12){
+            screenDisplay.textContent += "1";
+        }
+        secondNumber = screenDisplay.textContent;
+    });
+    two.addEventListener('click', e=>{
+        if(screenDisplay.textContent.length < 12){
+            screenDisplay.textContent += "2";
+        }
+        secondNumber = screenDisplay.textContent;
+    });
+    three.addEventListener('click', e=>{
+        if(screenDisplay.textContent.length < 12){
+            screenDisplay.textContent += "3";
+        }
+        secondNumber = screenDisplay.textContent;
+    });
+    four.addEventListener('click', e=>{
+        if(screenDisplay.textContent.length < 12){
+            screenDisplay.textContent += "4";
+        }
+        secondNumber = screenDisplay.textContent;
+    });
+    five.addEventListener('click', e=>{
+        if(screenDisplay.textContent.length < 12){
+            screenDisplay.textContent += "5";
+        }
+        secondNumber = screenDisplay.textContent;
+    });
+    six.addEventListener('click', e=>{
+        if(screenDisplay.textContent.length < 12){
+            screenDisplay.textContent += "6";
+        }
+        secondNumber = screenDisplay.textContent;
+    });
+    seven.addEventListener('click', e=>{
+        if(screenDisplay.textContent.length < 12){
+            screenDisplay.textContent += "7";
+        }
+        secondNumber = screenDisplay.textContent;
+    });
+    eight.addEventListener('click', e=>{
+        if(screenDisplay.textContent.length < 12){
+            screenDisplay.textContent += "8";
+        }
+        secondNumber = screenDisplay.textContent;
+    });
+    nine.addEventListener('click', e=>{
+        if(screenDisplay.textContent.length < 12){
+            screenDisplay.textContent += "9";
+        }
+        secondNumber = screenDisplay.textContent;
+    });
+
+    symbols.addEventListener('click', e=>{
+        if(screenDisplay.textContent.length < 12){
+            if(screenDisplay.textContent[0] == "-"){
+            screenDisplay.textContent = screenDisplay.textContent.substring(1);
+            }else{
+            screenDisplay.textContent = screenDisplay.textContent[0] = "-" + screenDisplay.textContent; 
+            }
+        }
+    });
+    dot.addEventListener('click', e=>{
+        if(screenDisplay.textContent.length < 12){
+            if(!screenDisplay.textContent.includes(".")){
+                screenDisplay.textContent += ".";
+                console.log("right!");
+            }
+        }
+    });
+
 
 //Operators
 AC.addEventListener('click', e=>{
     screenDisplay.textContent = "";
     firstNumber = "";
     secondNumber = "";
-});
-symbols.addEventListener('click', e=>{
-    if(screenDisplay.textContent[0] == "-"){
-        screenDisplay.textContent = screenDisplay.textContent.substring(1);
-    }else{
-        screenDisplay.textContent = screenDisplay.textContent[0] = "-" + screenDisplay.textContent; 
-    }
 });
 add.addEventListener('click', e=>{
     firstNumber = screenDisplay.textContent;
@@ -102,7 +133,6 @@ divide.addEventListener('click', e=>{
 });
 modular.addEventListener('click', e=>{
     firstNumber = screenDisplay.textContent;
-    console.log("mod");
     operator = "Modular";
     Operate(operator, firstNumber, secondNumber);
 });
